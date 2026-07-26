6c58f320495cab4cb85b5f3564c75b16e695e495
Fireyex <danfire999@gmail.com>
2026-07-25 19:44:15 -0500

    First upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// Configura la conexión apuntando a la dirección de tu backend .NET
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:5097/gamehub") // Asegúrate de verificar el puerto de tu servidor
+    .build();
+
+// Escucha el evento que configuramos en C#
+connection.on("UpdatePlayerCount", (count) => {
+    document.getElementById("player-count").textContent = count;
+});
+
+document.getElementById("temp").textContent = "Funciona"
+// Inicia la conexión en tiempo real
+async function start() {
+    try {
+        await connection.start();
+        console.log("¡Conectados a SignalR con éxito!");
+    } catch (err) {
+        console.error("Error al conectar:", err);
+        setTimeout(start, 5000); // Intenta reconectar en 5 segundos si falla
+    }
+}
+
+start();
